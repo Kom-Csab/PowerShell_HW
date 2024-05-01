07aa61bd4fe980bbd711b4785fc512f0f761e7ea
Kom-Csab <komcs0830@gmail.com>
2024-05-01 14:12:08 +0200

    The script can now write the results in an output file.

--- TIN_to_VTK_converter_main.ps1
+++ TIN_to_VTK_converter_main.ps1
@@ -1,11 +1,16 @@
 param (
     [Parameter(ValueFromRemainingArguments = $true)]
-    [string[]]$FilesFromParam
+    [string[]]$FilesFromParam,
+
+    [Parameter(Mandatory = $true)]
+    [string]$OutputFile
 )
+
+$outputContent = @()
 
 foreach ($file in $FilesFromParam){
     if (-not (Test-Path $file)){
-        Write-Error "A bemeneti fajl nem talalhato: $inputFile"
+        Write-Error "A bemeneti fajl nem talalhato: $file"
         continue
     }
 
@@ -46,9 +51,14 @@ foreach ($file in $FilesFromParam){
         elseif ($isTriProc -and $line -notmatch '^(BEGT|TNAM|MAT|TRI|ENDT|TIN)') {
             $tri_numbers += $line.Split(' ')
         }
+        else{if ($line -match "^ENDT") {
+            if ($title -eq ""){
+                $title = "vtk output"
+            }
+        }}
     }
 
-    $output = @"
+    $outputContent += @"
 # vtk DataFile Version 3.0
 $title
 ASCII
@@ -57,30 +67,33 @@ DATASET UNSTRUCTURED_GRID
 POINTS $vert_amount float
 "@
 
-    Write-Host $output
-    foreach ($vert_num in $vert_numbers){
-        Write-Host -NoNewline "$vert_num "
-        $count ++
-        if ($count -eq 3){
-            Write-Host ""
+    foreach ($num in $vert_numbers){
+        $vert_line += "$num "
+        $count++
+        if (($count % 3) -eq 0){
+            $outputContent += "$vert_line"
+            $vert_line = ""
             $count = 0
         }
     }
 
-    Write-Host ""
-    Write-Host "CELLS $tri_amount $tri_size"
+    $outputContent += ""
+    $outputContent += "CELLS $tri_amount $tri_size"
     for ($i = 0; $i -lt $tri_amount; $i++){
-        Write-Host -NoNewline "3 "
+        $tri_line += "3 "
         for ($j = 0; $j -lt 3; $j++){
-            Write-Host -NoNewline $tri_numbers[$i * 3 + $j]
-            Write-Host -NoNewline " "
+            $tri_line += "$($tri_numbers[$i * 3 + $j]) "
         }
-        Write-Host ""
+        $outputContent += $tri_line
+        $tri_line = ""
     }
 
-    Write-Host ""
-    Write-Host "CELL_TYPES $tri_amount"
+    $outputContent += ""
+    $outputContent += "CELL_TYPES $tri_amount"
     for ($i = 0; $i -lt $tri_amount; $i++){
-        Write-Host 5
+        $outputContent += "5"
     }
 }
+
+$outputContent | Set-Content -Path $OutputFile
+Write-Host "A kimeneti fajl sikeresen letrehozva: $OutputFile"
